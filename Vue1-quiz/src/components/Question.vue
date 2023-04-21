9201c3b33ae27d06b6ed0f430a0fb98bbaa9b08f
TROSPUD <1604574395@qq.com>
2023-04-21 22:31:28 +0800

    A quiz website created using Vue

---
+++ Vue1-quiz/src/components/Question.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="questions-ctr">
+        <div class="progress">
+            <div class="bar" :style="{ width: `${quesAns / ques.length * 100}%` }"></div>
+            <div class="status">{{ quesAns }} out of {{ ques.length }} questions answered</div>
+        </div>
+        <transition-group name="fade">
+        <div class="single-question" v-for="(que, index) in ques" :key="que.q" v-show="index === quesAns">
+            <div class="question">{{que.q}}</div>
+            <div class="answers" v-for="an in que.answers" :key="an.text">
+                <div class="answer" @click.prevent="selectAns(an.is_correct)">{{ an.text }}</div>
+            </div>
+        </div>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Question',
+        props: ['ques','quesAns'],
+        emits: ['select-event'],
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            selectAns(is_correct) {
+                this.$emit('select-event',is_correct);
+                console.log('select event')
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Vue1-quiz/src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="result">
+        <div class="title">{{ result[resultIndex].title }}</div>
+        <div class="desc">
+            {{ result[resultIndex].desc }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Result',
+        props: ['totalCorrect','result'],
+        computed: {
+            resultIndex() {
+                let index=0;
+                this.result.forEach((e, i) => {
+                    if(e.min <= this.totalCorrect && e.max >= this.totalCorrect) {
+                        index = i;
+                    }
+                });
+                return index;
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
